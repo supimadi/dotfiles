222c29f601704e4bbcddf09f911006056eb2767a
Lukmanhakim112 <hlukman156@gmail.com>
2021-06-29 13:05:38 +0700

    add splash images to doom emacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -71,6 +71,25 @@
 ;; Set src bin for rust
 (setq racer-rust-src-path "/home/lukman/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library")
 
+;; Here configuration for java meghanada
+;; beautify code before save
+(require 'meghanada)
+(add-hook 'java-mode-hook
+          (lambda ()
+            ;; meghanada-mode on
+            (meghanada-mode t)
+            (flycheck-mode +1)
+            (setq c-basic-offset 2)
+            ;; use code format
+            (add-hook 'before-save-hook 'meghanada-code-beautify-before-save)))
+(cond
+ ((eq system-type 'windows-nt)
+  (setq meghanada-java-path (expand-file-name "bin/java.exe" (getenv "JAVA_HOME")))
+  (setq meghanada-maven-path "mvn.cmd"))
+ (t
+  (setq meghanada-java-path "java")
+  (setq meghanada-maven-path "mvn")))
+
 ;; Remove encoding info in mode line
 (defun doom-modeline-conditional-buffer-encoding ()
   "We expect the encoding to be LF UTF-8, so only show the modeline when this is not the case"
